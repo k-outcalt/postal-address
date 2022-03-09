31396b3f1bbea32c47c48996f7dacf41e5ea0bb1
Katherine Outcalt <katherine.outcalt@gmail.com>
2022-03-08 20:41:34 -0800

    script for db population

--- db/transform-to-addy.py
+++ db/transform-to-addy.py
@@ -1,27 +1,41 @@
 """transform-to-addy.py
-Transforms a geojson into a json address that we can store in
-out database
+Transforms a geojson into a json address to store in 
+database
 
 Note: because of difficulty with importing packages, 
 each line is transformed into a string then back to a hash.
 This is likely to impact performance greatly
+
+Using insert_one instead of insert many
 """
 import json
+from socket import AddressInfo
 import transform
+import pymongo
 
 RAW_ADDRESSES = 'sample_group.geojson'
+MONGO_CONNECTION_STR = 'mongodb+srv://outcaltk-cluster:rjOBFu5GAKmsSGZ3@addresses.4q6sq.mongodb.net/addresses?retryWrites=true&w=majority'
+ADDR_LIMIT = 1500
 
-def main(count):
+def populate_cluster(limit):
+    # connect to MongoDB
+    count = 0
+    client = pymongo.MongoClient(MONGO_CONNECTION_STR)
+    db = client.addressDB
+    addrs = db.address
+
+    # geojson => json => insert document
     with open(RAW_ADDRESSES) as file:
-        
         for line in file:
             item = json.loads(line)
-            print(transform.create_hash_US_WA(item))
-            count -= 1
-            print("count is: ", count)
-            if count < 1:
+            addr = transform.create_hash_US_WA(item)
+            addrs.insert_one(addr)
+
+            count += 1
+            if count >= limit:
                 break
 
+
 if __name__ == "__main__":
-    main(10)
+    populate_cluster(ADDR_LIMIT)
 
--- db/transform.py
+++ db/transform.py
@@ -3,21 +3,33 @@ module for transforming the raw data into hashes for
 team's NoSQL collection
 """
 
+ID = '_id'
+COUNTRY = 'country'
+TERRITORY = 'territory'
+CITY = 'city'
+POSTCODE = 'postcode'
+STREET = 'street'
+UNIT = 'unit'
+NO = 'number' 
+
+PROP = 'properties'
+
 # TODO: Create hash for every required country-Kat
+# Brazil, Canada, Germany, India, Japan, North and South Korea, Mexico, Spain, UK, USA
 
 def create_hash_US_WA(gj):
     """
     >>> create_hash_US_WA({"type":"Feature","properties":{"hash":"a61bdd50a9646a04","number":"119","street":"NW  41ST ST","unit":"","city":"SEATTLE","district":"KING","region":"","postcode":"98107","id":"324731.0"},"geometry":{"type":"Point","coordinates":[-122.3580529,47.6561133]}})
     {'_id': 'a61bdd50a9646a04', 'country': 'US', 'territory': 'WA', 'city': 'SEATTLE', 'postcode': '98107', 'street': 'NW  41ST ST', 'unit': '', 'number': '119'}
     """
-    new_hash = {
-        "_id": gj["properties"]["hash"],
-        "country": "US",
-        "territory": "WA",
-        "city": gj["properties"]["city"],
-        "postcode": gj["properties"]["postcode"],
-        "street": gj["properties"]["street"],
-        "unit": gj["properties"]["unit"],
-        "number": gj["properties"]["number"]
+    return {
+        ID: gj[PROP]["hash"],
+        COUNTRY: "US",
+        TERRITORY: "WA",
+        CITY: gj[PROP]["city"],
+        POSTCODE: gj[PROP]["postcode"],
+        STREET: gj[PROP]["street"],
+        UNIT: gj[PROP]["unit"],
+        NO: gj[PROP]["number"]
     }
-    return new_hash
+
